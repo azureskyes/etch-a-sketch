ee05b43415f053c1ed9405f7ebaae36f5745f9e6
Clark Ocampo <ccocampo.code@gmail.com>
2024-03-04 01:03:08 -0800

    JS refinement. added button functionality, color, erase, reset, slider functionality.

--- script-eas.js
+++ script-eas.js
@@ -1,38 +1,3 @@
-
-
-/**function createDivGroup(group, gridNum) {
-let group =  1;
-let g = group;
-let gridNum = 5;
-let gN = gridNum;
-
-for (let g = 0; g < g; g++) {
-    for (let i = 0; i < gN; i++) {
-    let newDiv = document.createElement("div");
-    newDiv.className = ("div-" + (i + 1));
-    newDiv.textContent = (i + 1);
-    let canvasContainer = document.querySelector(".canvas-container");
-    canvasContainer.appendChild(newDiv);
-    }
-}
-}
-**/
-
-
-document.querySelector('#gridForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    let gridNum = document.querySelector("#gridInput").value;
-    let gridNumSqRt = Math.sqrt(gridNum);
-    let isPerfectSquare = gridNumSqRt === parseInt(gridNumSqRt);
-    if (isPerfectSquare) {
-        console.log('Grid Size input is a square root. Done.', 'Pixel count:', gridNum);
-        createGrid(gridNum);
-    } else {
-        console.log('Grid Size input is not a square root. Defaulting to 256 pixels / 16x16.');
-        createGrid(256);
-    }
-});
-
 function clearCanvas() {
     let parentDiv = document.querySelector('.canvas-container')
     let childDivs = parentDiv.querySelectorAll('div');
@@ -49,8 +14,6 @@ function createGroup(groupSize) {
         console.log(i);
     }
 }
-
-/*createGroup(16);*/
 
 function createColumn(columnSize) {
     let cSize = columnSize; 
@@ -71,6 +34,8 @@ document.addEventListener('mouseup', () => {
     isDrawing = false;
 });
 
+function grayscaleEtch () {
+let isGrayscale = true;
 document.querySelector('.canvas-container').addEventListener('mousedown', (e) =>{
     if (e.target.classList.contains('div-pixel')) {
             e.target.style.backgroundColor = 'gray';
@@ -93,6 +58,81 @@ document.querySelectorAll('.div-pixel').forEach(pixel => {
         e.target.style.backgroundColor = 'gray';
     });
 });
+}
+grayscaleEtch();
+
+function rainbowEtch () {
+    let isRainbow = true;
+
+    document.querySelector('.canvas-container').addEventListener('mousedown', (e) =>{
+        if (e.target.classList.contains('div-pixel')) {
+            e.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+        }
+    });
+    document.querySelector('.canvas-container').addEventListener('mouseover', (e) =>{
+        if (e.target.classList.contains('div-pixel')) {
+            e.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+        }
+    });
+    
+    document.querySelectorAll('.div-pixel').forEach(pixel => {
+        pixel.addEventListener('mousedown', (e) => {
+            e.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+        });
+    });
+    
+    document.querySelectorAll('.div-pixel').forEach(pixel => {
+        pixel.addEventListener('mouseover', (e) => {
+            e.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+        });
+    });
+    }
+
+function eraserEtch () {
+        let isEraser = true;
+        document.querySelector('.canvas-container').addEventListener('mousedown', (e) =>{
+            if (e.target.classList.contains('div-pixel')) {
+                    e.target.style.backgroundColor = '';
+            }
+        });
+        document.querySelector('.canvas-container').addEventListener('mouseover', (e) =>{
+            if (e.target.classList.contains('div-pixel')) {
+                    e.target.style.backgroundColor = '';
+            }
+        });
+        
+        document.querySelectorAll('.div-pixel').forEach(pixel => {
+            pixel.addEventListener('mousedown', (e) => {
+                    e.target.style.backgroundColor = '';
+            });
+        });
+        
+        document.querySelectorAll('.div-pixel').forEach(pixel => {
+            pixel.addEventListener('mouseover', (e) => {
+                    e.target.style.backgroundColor = '';
+            });
+        });
+        }
+
+function resetEtch() {
+    createGrid(256);
+    let slider = document.querySelector('.gridSlider');
+    slider.value = 2;
+    console.log('Canvas has been reset to default 16x16.');
+}
+
+document.querySelector('.grayscale').addEventListener('click', (e) => {
+    grayscaleEtch();
+});
+document.querySelector('.rainbow').addEventListener('click', (e) => {
+    rainbowEtch();
+});
+document.querySelector('.eraser').addEventListener('click', (e) => {
+    eraserEtch();
+});
+document.querySelector('.resetAll').addEventListener('click', (e) => {
+    resetEtch();
+});
 
 function createGrid(totalPixels) {
     const container = document.querySelector('.canvas-container');
@@ -113,35 +153,71 @@ function createGrid(totalPixels) {
     }
 }
 
-createGrid(16*16);
+createGrid(256);
+let slider = document.querySelector('.gridSlider');
+slider.value = 2;
+
+const sliderOptions = 
+[9, 16, 25, 36, 49,
+64, 81, 100, 121, 144,
+169, 196, 225, 256, 289,
+324, 361, 400, 441, 484,
+529, 576, 625, 676, 729,
+784, 841, 900, 961, 1024];
 
 document.addEventListener('DOMContentLoaded', () => {
     let slider = document.querySelector('.gridSlider');
     let display = document.getElementById('sliderValue');
 
-    const sliderOptions = 
-    [9, 16, 25, 36, 49,
-    64, 81, 100, 121, 144,
-    169, 196, 225, 256, 289,
-    324, 361, 400, 441, 484,
-    529, 576, 625, 676, 729,
-    784, 841, 900, 961, 1024];
-
     slider.addEventListener('input', (e) => {
         const index = parseInt(e.target.value) - 1;
         const selectedValue = sliderOptions[index];
         display.textContent = selectedValue;
-    })
+        let gridNum = selectedValue;
+        console.log('Grid Size input is a perfect square. Done.', 'Pixel count:', gridNum);
+        createGrid(gridNum);
+    })});
 
-    document.querySelector('#gridForm').addEventListener('submit', (e) => {
-        event.preventDefault();
-        const index = parseInt(slider.value) - 1;
-        let gridNum = sliderOptions[index];
 
+
+
+/**function createDivGroup(group, gridNum) {
+let group =  1;
+let g = group;
+let gridNum = 5;
+let gN = gridNum;
+
+for (let g = 0; g < g; g++) {
+    for (let i = 0; i < gN; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.className = ("div-" + (i + 1));
+    newDiv.textContent = (i + 1);
+    let canvasContainer = document.querySelector(".canvas-container");
+    canvasContainer.appendChild(newDiv);
+    }
+}
+}
+**/
+
+
+
+
+
+/*
+document.querySelector('#gridForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    let gridNum = document.querySelector("#gridInput").value;
+    let gridNumSqRt = Math.sqrt(gridNum);
+    let isPerfectSquare = gridNumSqRt === parseInt(gridNumSqRt);
+    if (isPerfectSquare) {
         console.log('Grid Size input is a square root. Done.', 'Pixel count:', gridNum);
         createGrid(gridNum);
-    })
-})
+    } else {
+        console.log('Grid Size input is not a square root. Defaulting to 256 pixels / 16x16.');
+        createGrid(256);
+    }
+}); */
+
 
 /**let isDrawing = false;
 document.addEventListener('mousedown', () => {
@@ -211,4 +287,4 @@ function createGroup(groupSize) {
     }
 }
 
-*/
+**/;
